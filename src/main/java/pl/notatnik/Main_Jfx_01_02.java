e9d3c0b347a7f7c637218c3311df199d3e7189c3
kohrd <konrad.janeczek@protonmail.com>
2019-01-24 14:47:55 +0100

    initial

---
+++ src/main/java/pl/notatnik/Main_Jfx_01_02.java
@@ -0,0 +1,24 @@
+package pl.notatnik;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main_Jfx_01_02  extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/fxmlDocument.fxml"));
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
